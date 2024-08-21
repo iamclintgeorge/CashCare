a4141a0df0c2ba40777984e629b2191e713ea465
Sahil Joshi <sahiljoshi342.com>
2024-08-21 17:47:01 +0530

    edited js code

--- app/Components/Header.js
+++ app/Components/Header.js
@@ -1,10 +1,22 @@
 import React from "react";
 
+
 const Header = () => {
   return (
     <>
-      <div>This is Header Section</div>
-    </>
+    <ul className="divul" >
+    <li className="ul"><a href="home">HOME</a></li>
+    <li className="ul"><a href="pricing">PRICING</a></li>
+    <li className="ul"><a href="products" >PRODUCTS</a></li>
+    <li className="ul"><a href="resources" >RESOURCES</a></li>
+    <li className="ul"><a href="contact" >CONTACT US</a></li>
+    <li className="ul"><button className=" button" ><a  href="sign">Sign In</a></button></li>
+    </ul>
+    
+
+</>
+      
+    
   );
 };
 
--- app/Pages/Signup.jsx
+++ app/Pages/Signup.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const Signup = () => {
   return <div>Signup to my Account Please</div>;
+  <div>this </div>
 };
 
 export default Signup;
