58517a6501b4b357830ba6b598869791111d9260
Clint George <clintbgeorge@gmail.com>
2024-09-02 20:57:55 +0530

    Made some minor changes
    Signed-off-by: Clint George <clintbgeorge@gmail.com>

--- app/Components/Header.js
+++ app/Components/Header.js
@@ -1,22 +1,26 @@
 import React from "react";
 
-
 const Header = () => {
   return (
     <>
-    <ul className="divul" >
-    <li className="ul"><a href="home">HOME</a></li>
-    <li className="ul"><a href="pricing">PRICING</a></li>
-    <li className="ul"><a href="products" >PRODUCTS</a></li>
-    <li className="ul"><a href="resources" >RESOURCES</a></li>
-    <li className="ul"><a href="contact" >CONTACT US</a></li>
-    <li className="ul"><button className=" button" ><a  href="sign">Sign In</a></button></li>
-    </ul>
-    
-
-</>
-      
-    
+      <div className="mt-10 flex justify-around">
+        <div className="font-extrabold tracking-widest text-lg leading-5">
+          <p>CASH</p>
+          <p>CARE</p>
+        </div>
+        <ul className="flex gap-10 items-center text-sm tracking-widest">
+          <li>HOME</li>
+          <li>PRICING</li>
+          <li>PRODUCTS</li>
+          <li>RESOURCES</li>
+          <li>CONTACT US</li>
+        </ul>
+        <ul className="flex gap-5 items-center text-xs tracking-widest">
+          <li className="border rounded-2xl px-5 py-2 border-black">SIGN IN</li>
+          <li>EN</li>
+        </ul>
+      </div>
+    </>
   );
 };
 
