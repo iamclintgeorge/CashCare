9b24aeffb8069d99f9dc0ad0c4c077a2ef1d03ae
shubham karne <120401166+shubhamkarne11@users.noreply.github.com>
2024-09-04 00:04:40 +0530

    changes made in price section

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Header from "./Components/Header";
 import Herosec from "./Sections/Herosec";
 import Whycashcare from "./Sections/Whycashcare";
+import Pricing from "./Sections/Pricing";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <Herosec />
       <Whycashcare />
+      <Pricing />
     </main>
   );
 }
