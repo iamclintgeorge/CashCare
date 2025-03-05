ed378d5b4c458a2cac52d8d8fb1117284d80c2ed
Clint George <clintbgeorge@gmail.com>
2025-03-05 23:03:27 +0530

    Fixed Geolocation feature
    Signed-off-by: Clint George <clintbgeorge@gmail.com>

--- CashCare/networksniffer.h
+++ CashCare/networksniffer.h
@@ -6,21 +6,18 @@
 #include <memory>
 #include <pcap/pcap.h>
 #include <QNetworkAccessManager>
+#include <QNetworkReply>
 
 class NetworkSniffer : public QObject {
     Q_OBJECT
     Q_PROPERTY(QString packetInfo READ packetInfo NOTIFY packetInfoChanged)
     Q_PROPERTY(int totalPackets READ totalPackets NOTIFY totalPacketsChanged)
-    Q_PROPERTY(int blockedPackets READ blockedPackets NOTIFY blockedPacketsChanged)
-    Q_PROPERTY(qreal bandwidthUsage READ bandwidthUsage NOTIFY bandwidthUsageChanged)
 public:
     explicit NetworkSniffer(QObject *parent = nullptr);
     ~NetworkSniffer();
 
     QString packetInfo() const { return m_packetInfo; }
     int totalPackets() const { return m_totalPackets; }
-    int blockedPackets() const { return m_blockedPackets; }
-    qreal bandwidthUsage() const { return m_bandwidthUsage; }
 
 public slots:
     void startSniffing();
@@ -29,20 +26,18 @@ public slots:
 signals:
     void packetInfoChanged();
     void totalPacketsChanged();
-    void blockedPacketsChanged();
-    void bandwidthUsageChanged();
 
 private slots:
     void capturePacket();
+    void handleGeoReply(QNetworkReply *reply); // Handle API response
 
 private:
     pcap_t *m_pcapHandle;
     std::unique_ptr<QTimer> m_timer;
     QString m_packetInfo;
     int m_totalPackets = 0;
-    int m_blockedPackets = 0;
-    qreal m_bandwidthUsage = 0.0;
     QNetworkAccessManager *m_networkManager;
+    QString m_pendingIp; // Store IP awaiting geolocation
 };
 
 #endif
--- CashCare/packetparser.cpp
+++ CashCare/packetparser.cpp
@@ -1,6 +1,8 @@
 #include "packetparser.h"
 #include "networkutils.h"
 #include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
 
 #ifdef _WIN32
 #include <winsock2.h>
@@ -34,9 +36,30 @@ QString PacketParser::parsePacket(const struct pcap_pkthdr *header, const u_char
         return packetDetails + "Not an IP packet";
     }
 
+    int ipHeaderLen = ipHeader->ip_hl * 4;
     packetDetails += QString("IP Header:\n");
     packetDetails += QString("  Source IP: %1\n").arg(NetworkUtils::ipToString(ipHeader->ip_src));
     packetDetails += QString("  Destination IP: %1\n").arg(NetworkUtils::ipToString(ipHeader->ip_dst));
+    packetDetails += QString("  Protocol: %1\n").arg(ipHeader->ip_p);
 
+    if (header->caplen < sizeof(struct ether_header) + ipHeaderLen) {
+        return packetDetails + "Packet too short for transport header";
+    }
+
+    if (ipHeader->ip_p == IPPROTO_TCP) {
+        struct tcphdr *tcpHeader = (struct tcphdr *)(packetData + sizeof(struct ether_header) + ipHeaderLen);
+        packetDetails += QString("TCP Header:\n");
+        packetDetails += QString("  Source Port: %1\n").arg(ntohs(tcpHeader->th_sport));
+        packetDetails += QString("  Destination Port: %1\n").arg(ntohs(tcpHeader->th_dport));
+        packetDetails += QString("  Flags: %1\n").arg(tcpHeader->th_flags, 0, 16);
+        packetDetails += QString("  Window Size: %1\n").arg(ntohs(tcpHeader->th_win));
+    } else if (ipHeader->ip_p == IPPROTO_UDP) {
+        struct udphdr *udpHeader = (struct udphdr *)(packetData + sizeof(struct ether_header) + ipHeaderLen);
+        packetDetails += QString("UDP Header:\n");
+        packetDetails += QString("  Source Port: %1\n").arg(ntohs(udpHeader->uh_sport));
+        packetDetails += QString("  Destination Port: %1\n").arg(ntohs(udpHeader->uh_dport));
+    }
+
+    packetDetails += QString("Payload Length: %1\n").arg(header->caplen - (sizeof(struct ether_header) + ipHeaderLen));
     return packetDetails;
 }
